2ed198e6f221111e5b25d838a3de9e516dad63f2
Jawaher AlMuwaiji <89389146+JawaherMuwaijie@users.noreply.github.com>
2022-05-28 15:17:20 +0100

    Create figlet.py

---
+++ figlet.py
@@ -0,0 +1,13 @@
+from pyfiglet import Figlet
+
+figlet = Figlet()
+n = input ("Input: ")
+figlet= figlet. figletez
+print ("Output: ", figlet)
+figlet.getFonts()
+figlet.setFont(font=f)
+print(figlet.renderText(s))
+if
+    print("Invalid usage")
+if font not -f --font
+    sys.exit("Too few arguments")
